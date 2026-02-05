3623fcd71763d2639ac1b19c6ba910d8fd407ba9
Pranav Late <pranav.a.late99@gmail.com>
2026-02-05 14:10:17 +0530

    Refactor game menu to use a dictionary for programs

--- game-menu.py
+++ game-menu.py
@@ -1,55 +1,60 @@
 import subprocess
 from pathlib import Path
-
-print("Welcome!")
-print("Select")
-print("1 for Tic Tac Toe")
-print("2 for Guess The Number")
-print("3 for Sudoku")
-print("4 for Number Base Convertor")
-print("5 for Calculator")
-print("6 to Quit")
 script_dir = Path(__file__).parent.resolve()
-sudoku_file = script_dir/"games_file"/"sudoku.py"
-GTN_file = script_dir/"games_file"/"guess-the-number.py"
-TTT_file = script_dir/"games_file"/"tic-tac-toe.exe"
-NBC_file = script_dir/"games_file"/"number-base-convertor.py"
-calc_file = script_dir/"games_file"/"calculator.py"
+programs = {
+    1 : {
+        "name" : "Tic Tac Toe",
+        "path" : script_dir/"games_file"/"tic-tac-toe.exe",
+        "run_type" : "exe",
+        
+    },
+    2 : {
+        "name" : "Guess The Number",
+        "path" : script_dir/"games_file"/"guess-the-number.py",
+        "run_type" : "py",   
+    },
+    3 : {
+        "name" : "Sudoku",
+        "path" : script_dir/"games_file"/"sudoku.py",
+        "run_type" : "py",
+    },
+    4 : {
+        "name" : "Number Base Convertor",
+        "path" : script_dir/"games_file"/"number-base-convertor.py",
+        "run_type" : "py",
+    },
+    5 : {
+        "name" : "Calculator",
+        "path" : script_dir/"games_file"/"calculator.py",
+        "run_type" : "py",
+    }
+}
 
-while True:
-    try:
-        choice = int(input(""))
-    except ValueError:
-        print("Please enter a number")
-        continue
-    match choice:
-        case 1:
-            if TTT_file.exists():
-                subprocess.run([str(TTT_file)])
-            else:
-                print("Game not found")
-        case 2:
-            if GTN_file.exists():
-                subprocess.run(["python", str(GTN_file)])
-            else:
-                print("Game not found")
-        case 3:
-            if sudoku_file.exists():
-                subprocess.run(["python", str(sudoku_file)])
-            else:
-                print("Game not found")
-        case 4:
-            if NBC_file.exists():
-                subprocess.run(["python", str(NBC_file)])
-            else:
-                print("Game not found")
-        case 5:
-            if calc_file.exists():
-                subprocess.run(["python", str(calc_file)])
-            else:
-                print("Game not found")
-        case 6:
+print("Welcome")
+print("Select")
+for n in programs:
+    print(f'{n} for {programs[n]["name"]}')
+print("6 to Quit")
+
+
+def play(programs):
+    while True:
+        try:
+            choice = int(input(""))
+        except ValueError:
+            print("Please enter a number")
+            continue
+        if choice == 6:
             break
-        case _:
+        elif choice in programs:
+            if programs[choice]["path"].exists():
+                if programs[choice]["run_type"] == "exe":
+                    subprocess.run([str(programs[choice]["path"])])
+                elif programs[choice]["run_type"] == "py":
+                    subprocess.run(["python", programs[choice]["path"]])
+            else:
+                print("Game not found")
+        else:
             print("Enter number between 1 to 6")
     
+play(programs)
